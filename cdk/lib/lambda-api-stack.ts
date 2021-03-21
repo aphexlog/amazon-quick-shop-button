531b4567cdd7feb2469de129e8b3c3b44e9eec47
aphexlog <awest@fluentstream.com>
2021-03-20 20:55:23 -0500

    updating cdk lib

--- cdk/lib/lambda-api-stack.ts
+++ cdk/lib/lambda-api-stack.ts
@@ -1,9 +1,39 @@
-import * as cdk from '@aws-cdk/core';
+import { Construct, Duration, Stack, StackProps } from "@aws-cdk/core";
+import { LambdaIntegration, MethodLoggingLevel, RestApi } from "@aws-cdk/aws-apigateway";
+import { PolicyStatement } from "@aws-cdk/aws-iam";
+import { Function, Runtime, AssetCode, Code } from "@aws-cdk/aws-lambda";
 
-export class LambdaApiStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+
+interface ILambdaApiStackProps extends StackProps {
+  functionName: string
+}
+
+export class LambdaApiStack extends Stack {
+  private restApi: RestApi;
+  private lambdaFunction: Function;
+
+  constructor(scope: Construct, id: string, props?: ILambdaApiStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    this.restApi = new RestApi(this, 'RestApi', {
+      deployOptions: {
+        stageName: 'beta',
+        metricsEnabled: true,
+        loggingLevel: MethodLoggingLevel.INFO,
+        dataTraceEnabled: true
+      }
+    });
+
+    const lambdaPolicy = new PolicyStatement();
+
+    this.lambdaFunction = new Function(this, 'Function', {
+      functionName: props?.functionName,
+      handler: "handler.handler",
+      runtime: Runtime.NODEJS_14_X,
+      code: new AssetCode('../src'),
+      memorySize: 512,
+      timeout: Duration.seconds(30),
+      initialPolicy: [lambdaPolicy]
+    })
   }
 }
